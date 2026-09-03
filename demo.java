103298486d841e13238bce9f26e2af3b0ae72950
Abhijit Jadhav <2303151@ritindia.edu>
2026-09-03 14:19:53 +0530

    Update project file

--- demo.java
+++ demo.java
@@ -16,12 +16,12 @@ System.out.print("Soham-2303138");
 System.out.print("Om,2203139");
 System.out.println("name : Prathmesh Yadav");
 System.out.println("PRN : 2303146");
+System.out.println("Abhijeet-2303151");
 
 
 
 System.out.println("Atharv-2203021");
 System.out.print("Rohit-2203047");
-
 
 System.out.print("Shreya-2303150");
 
@@ -29,4 +29,4 @@ System.out.print("Shreya-2303150");
 
 System.out.print("RItik-2303149");
 }
-}
+
